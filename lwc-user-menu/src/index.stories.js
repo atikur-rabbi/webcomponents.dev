7f934ecc702f8754a70701ed1c5bc62b49fa1d67
Atikur Rabbi <13056586+undefined@users.noreply.github.com>
2022-10-25 10:15:53 +0000

    Push from lwc-user-menu

--- lwc-user-menu/src/index.stories.js
+++
@@ -1,7 +0,0 @@
-import "@lwc/synthetic-shadow";
-// import "https://unpkg.com/@salesforce-ux/design-system@2.13.1/assets/styles/salesforce-lightning-design-system.min.css";
-import "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap-grid.min.css"
-import { createElement } from "lwc";
-import App from "./app";
-
-export const story = () => createElement("c-app", { is: App });
